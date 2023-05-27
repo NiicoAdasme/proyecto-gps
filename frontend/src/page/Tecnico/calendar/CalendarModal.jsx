c4582cdbf884a6a98f2b3b89906805ce4b13707c
NiicoAdasme <nicolas.wladimir@gmail.com>
2023-05-27 00:01:49 -0400

    calendar features

--- frontend/src/page/Tecnico/calendar/CalendarModal.jsx
+++ frontend/src/page/Tecnico/calendar/CalendarModal.jsx
@@ -6,6 +6,7 @@ import Swal from 'sweetalert2';
 import { useDispatch, useSelector } from 'react-redux';
 import { eventAddNew, eventClearActive, eventUpdated } from '../../../actions/events';
 import { uiCloseModal } from '../../../actions/ui';
+import { CustomModal } from '../../../components';
 
 
 const customStyles = {
@@ -130,79 +131,100 @@ export const CalendarModal = () => {
 
     }, [activeEvent, setFormValues])
 
+
     return (
-        <Modal
-            isOpen={modalOpen}
-            onAfterOpen={afterOpenModal}
-            onRequestClose={closeModal}
-            style={customStyles}
-            className="modal"
-            overlayClassName="modal-fondo"
-            closeTimeoutMS={200}
-        >
+        <CustomModal isOpen={modalOpen} titulo={ activeEvent ? 'Editar Evento' : 'Nuevo Evento'} onClose={closeModal} > 
+            <div className='space-y-2'>
+                <form className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl" onSubmit={handleSubmit}>
 
-            <h1>{ activeEvent ? 'Editar Evento' : 'Nuevo Evento' }</h1>
-            <hr />
-            <form className="container" onSubmit={handleSubmit}>
+                    <div className="mb-4 space-y-2">
+                        <label className='font-bold'>Fecha y hora inicio</label>
+                        {/* <DateTimePicker
+                            onChange={handleStartDateChange}
+                            value={start}
+                            className="form-control font-bold"
+                            
+                        /> */}
+                        <input 
+                            type="datetime-local"
+                            name="start"
+                            onChange={handleStartDateChange}
+                            value={start}
+                            className='form-control font-bold'
+                        />
+                    </div>
 
-                <div className="form-group">
-                    <label>Fecha y hora inicio</label>
-                    <DateTimePicker
-                        onChange={handleStartDateChange}
-                        value={start}
-                        className="form-control"
-                        
-                    />
-                </div>
+                    <div className="mb-4 space-y-2">
+                        <label className='font-bold'>Fecha y hora fin</label>
+                        {/* <DateTimePicker
+                            onChange={handleEndDateChange}
+                            value={end}
+                            className="form-control font-bold"
+                            minDate={start}
+                        /> */}
+                        <input 
+                            type="datetime-local"
+                            name="end"
+                            id=""
+                            onChange={e => handleEndDateChange(e)}
+                            value={end}
+                            className='form-control font-bold'
+                            min={start}
+                        />
+                    </div>
 
-                <div className="form-group">
-                    <label>Fecha y hora fin</label>
-                    <DateTimePicker
-                        onChange={handleEndDateChange}
-                        value={end}
-                        className="form-control"
-                        minDate={start}
-                    />
-                </div>
+                    <hr />
+                    <div className="mb-4 space-y-2">
+                        <label className='font-bold'>Titulo y notas</label>
+                        <input
+                            type="text"
+                            className={`form-control ${ !titleValid && 'is-invalid'} `}
+                            placeholder="Título del evento"
+                            name="title"
+                            autoComplete="off"
+                            value={title}
+                            onChange={handleInputChange}
+                        />
 
-                <hr />
-                <div className="form-group">
-                    <label>Titulo y notas</label>
-                    <input
-                        type="text"
-                        className={`form-control ${ !titleValid && 'is-invalid'} `}
-                        placeholder="Título del evento"
-                        name="title"
-                        autoComplete="off"
-                        value={title}
-                        onChange={handleInputChange}
-                    />
-                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
-                </div>
+                    </div>
 
-                <div className="form-group">
-                    <textarea
-                        type="text"
-                        className="form-control"
-                        placeholder="Notas"
-                        rows="5"
-                        name="notes"
-                        value={notes}
-                        onChange={handleInputChange}
-                    ></textarea>
-                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
-                </div>
+                    <div className="mb-4 space-y-2">
+                        <label className='font-bold'>Una descripción corta</label>
+                        <textarea
+                            type="text"
+                            className="form-control "
+                            placeholder="Notas"
+                            rows="5"
+                            name="notes"
+                            value={notes}
+                            onChange={handleInputChange}
+                        ></textarea>
+                        {/* <small id="emailHelp" className="text-sm text-gray-500 font-bold">Información adicional</small> */}
+                    </div>
 
-                <button
-                    type="submit"
-                    className="btn btn-outline-primary btn-block"
-                >
-                    <i className="far fa-save"></i>
-                    <span> { activeEvent ? 'Actualizar' : 'Guardar'} </span>
-                </button>
+                    <button
+                        type="submit"
+                        className="bg-paletaAzul3 border border-blue-500 text-white hover:bg-paletaAzul2 hover:text-white font-bold py-2 px-4 rounded-full w-full"
+                    >
+                        <i className="far fa-save"></i>
+                        <span> { activeEvent ? 'Actualizar' : 'Guardar'} </span>
+                    </button>
 
-            </form>
+                </form>
+            </div>
+        </CustomModal>
 
-        </Modal>
+        // <Modal
+        //     isOpen={modalOpen}
+        //     onAfterOpen={afterOpenModal}
+        //     onRequestClose={closeModal}
+        //     style={customStyles}
+        //     className="modal"
+        //     overlayClassName="modal-fondo"
+        //     closeTimeoutMS={200}
+        // >
+            
+
+        //  </Modal> 
     )
 }
