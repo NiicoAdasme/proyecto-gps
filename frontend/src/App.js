ae10a4d4618d98a9217a9aeebd5bef034cf2e56e
Naru9909 <narux99zx@gmail.com>
2023-05-26 20:39:50 -0400

    Redux Toolkit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,43 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
-import { NavBar } from "./components";
-import IncidenciasTecnico from "./page/Tecnico/Incidencias";
-import "./GlobalCSS.css";
-import TareasTecnico from "./page/Tecnico/Tareas/TareasTecnico";
-import useLoading from "./queries/Loading/useLoading";
-import LoadingMask from "./queries/Loading/LoadingMask";
-import Login from "./page/Login/Login";
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+  RouterProvider
+} from 'react-router-dom'
+import { NavBar } from './components'
+import IncidenciasTecnico from './page/Tecnico/Incidencias'
+import './GlobalCSS.css'
+import TareasTecnico from './page/Tecnico/Tareas/TareasTecnico'
+import useLoading from './queries/Loading/useLoading'
+import LoadingMask from './queries/Loading/LoadingMask'
+import Login from './page/Login/Login'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const LinkTecnico = [
-    { name: "Incidencias", link: "/incidencias" },
-    { name: "Tareas", link: "/tareas" },
-  ];
+    { name: 'Incidencias', link: '/incidencias' },
+    { name: 'Tareas', link: '/tareas' },
+  ]
 
   const [isLogged, setIsLogged] = useState(true);
 
   const loading = useLoading().data;
 
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route path="login" element={<Login />} />)
-  );
+    createRoutesFromElements(<Route path='login' element={<Login />} />)
+  )
 
   const routerLoged = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<NavBar options={LinkTecnico} />}>
-        <Route path="incidencias" element={<IncidenciasTecnico />} />
-        <Route path="tareas" element={<TareasTecnico />} />
-        <Route path="miPerfil" element={<></>} />
+      <Route path='/' element={<NavBar options={LinkTecnico} />}>
+        <Route path='incidencias' element={<IncidenciasTecnico />} />
+        <Route path='tareas' element={<TareasTecnico />} />
+        <Route path='miPerfil' element={<></>} />
       </Route>
     )
-  );
+  )
 
   return (
     <>
@@ -49,7 +49,7 @@ function App() {
         <RouterProvider router={router} />
       )}
     </>
-  );
+  )
 }
 
 export default App;
