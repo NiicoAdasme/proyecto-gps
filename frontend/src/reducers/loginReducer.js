7f47e88dd7e656f52bdc6098e994f9ef3a4fc42a
NiicoAdasme <nicolas.wladimir@gmail.com>
2023-05-26 20:58:53 -0400

    se quito el slint

--- frontend/src/reducers/loginReducer.js
+++ frontend/src/reducers/loginReducer.js
@@ -1,26 +1,25 @@
 const initialState = {
-    isLogged: false,
-    userData: null,
-  };
-  
-  const loginReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'LOGIN':
-        return {
-          ...state,
-          isLogged: true,
-          userData: action.payload,
-        };
-      case 'LOGOUT':
-        return {
-          ...state,
-          isLogged: false,
-          userData: null,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default loginReducer;
-  
+  isLogged: false,
+  userData: null,
+};
+
+const loginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        isLogged: true,
+        userData: action.payload,
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        isLogged: false,
+        userData: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default loginReducer;
