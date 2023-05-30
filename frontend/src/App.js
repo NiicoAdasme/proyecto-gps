6d67aebc0da50212a90da1d97917924a1a80be30
NiicoAdasme <nicolas.wladimir@gmail.com>
2023-05-30 00:13:31 -0400

    fixc app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,37 +7,13 @@ import useLoading from './queries/Loading/useLoading'
 import LoadingMask from './queries/Loading/LoadingMask'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import {routerLoged,router} from './page/pageRoutes';
-
-function App() {
-=======
+import { routerLoged, router } from './page/pageRoutes';
 import { CalendarScreen } from './page/Tecnico/calendar/CalendarScreen'
 
 function App() {
-  const LinkTecnico = [
-    { name: 'Incidencias', link: '/incidencias' },
-    { name: 'Tareas', link: '/tareas' },
-    { name: 'Calendario', link: '/calendario' },
-  ]
 
   const [isLogged, setIsLogged] = useState(true);
   const loading = useLoading().data;
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(<Route path='login' element={<Login />} />)
-  )
-
-  const routerLoged = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<NavBar options={LinkTecnico} />}>
-        <Route path='incidencias' element={<IncidenciasTecnico />} />
-        <Route path='tareas' element={<TareasTecnico />} />
-        <Route path='miPerfil' element={<></>} />
-        <Route path='calendario' element={<CalendarScreen/>} />
-      </Route>
-    )
-  )
-
 
   return (
     <>
