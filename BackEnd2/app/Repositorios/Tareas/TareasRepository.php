200d5250ee329c1f7905ccba1ac5f617f3a0cd92
estebanSH <esteban.soto@syscode.cloud>
2023-07-21 22:04:43 -0400

    se modifica api tareaDetalle

--- BackEnd2/app/Repositorios/Tareas/TareasRepository.php
+++ BackEnd2/app/Repositorios/Tareas/TareasRepository.php
@@ -20,18 +20,19 @@ class TareasRepository
             return $this->errorResponse("No se logro crear mensaje", 409, $ex, __METHOD__);
         }
     }
-    public function allTareas($request) {
+    public function allTareas($request)
+    {
         try {
-            $tareas = Tarea :: select()
-            ->with(
-                "usuario:id,usua_nombre,usua_apellido_p",
-                "estado:id,esta_nombre"
-            )
-            ->when($request?->fechaInicio,function ($q) use($request){
-                $q->whereDate('tare_fecha_inicio',$request->fechaInicio);
-            })
-            ->orderBy("id", "DESC")
-            ->paginate($request->perPage);
+            $tareas = Tarea::select()
+                ->with(
+                    "usuario:id,usua_nombre,usua_apellido_p",
+                    "estado:id,esta_nombre"
+                )
+                ->when($request?->fechaInicio, function ($q) use ($request) {
+                    $q->whereDate('tare_fecha_inicio', $request->fechaInicio);
+                })
+                ->orderBy("id", "DESC")
+                ->paginate($request->perPage);
             $tareas->getCollection()->transform(function ($tarea) {
                 $respuesta = [
                     "id" => $tarea->id,
@@ -44,20 +45,37 @@ class TareasRepository
             });
             return $this->successResponse($tareas, "Tareas listadas correctamente");
         } catch (Exception $ex) {
-            return $this->errorResponse("",409,$ex,__METHOD__);
+            return $this->errorResponse("", 409, $ex, __METHOD__);
         }
     }
-    public function tareaDetail($request) {
+    public function tareaDetail($request)
+    {
         try {
-            Log::info($request->id);
-            $tareas = Tarea :: find($request?->id);
-            return $this->successResponse($tareas, "Detalle de tarea listado correctamente");
+            $tarea = Tarea::with([
+                'usuario:id,usua_nombre,usua_apellido_p',
+                'areaPlanta:id,arpl_nombre'
+            ])->findOrFail($request->id);
+            //         $tareas = Tarea :: find($request?->id);
+            //         return $this->successResponse($tareas, "Detalle de tarea listado correctamente");
+            $respuesta = [
+                "id" => $tarea->id,
+                "titulo" => $tarea->tare_titulo,
+                "estado" => $tarea->estado->esta_nombre,
+                "usuario" => $tarea->usuario->usua_nombre . " " . $tarea->usuario->usua_apellido_p,
+                "areaPlanta" => $tarea->areaPlanta->arpl_nombre,
+                "fechaInicio" => $tarea->tare_fecha_inicio,
+                "fechaFin" => $tarea->tare_fecha_fin,
+                "descripcion" => $tarea->tare_descripcion
+            ];
+            return $this->successResponse($respuesta, "Detalle de tarea listado correctamente");
         } catch (Exception $ex) {
-            return $this->errorResponse("Se cayó buscando detalles",409,$ex,__METHOD__);
+            return $this->errorResponse("Se cayó buscando detalles", 409, $ex, __METHOD__);
         }
     }
 
-    private function ingresarDatos($data, Tarea $tarea = new Tarea()){
+
+    private function ingresarDatos($data, Tarea $tarea = new Tarea())
+    {
 
         $tarea->tare_titulo = $data->tare_titulo ?? $tarea->tare_titulo;
         $tarea->tare_descripcion = $data->tare_descripcion ?? $tarea->tare_descripcion;
@@ -72,6 +90,4 @@ class TareasRepository
 
         return $tarea;
     }
-
-    
 }
